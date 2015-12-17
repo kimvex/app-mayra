749c7463523ecdeaaeb5748870ddbc205f9f00e2
Benjamin <chomi.numb.94@gmail.com>
2015-12-17 14:00:58 -0600

    Agregamos mejoras al proyecto

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded',function(){
 
 	function conectar(e){
-		document.getElementById('action').innerHTML = '<object class="servervid" data="http://192.168.1.66:5000" type="text/html"></object>';
+		var ip = document.getElementById('ip').value;
+		console.log(ip)
+		/*$.get('http://' + ip,function(data){
+			document.getElementById('action').innerHTML = data;
+		});*/
+		document.getElementById('action').innerHTML = '<object class="servervid" data="http://192.168.1.'+ ip +':5000" type="text/html"></object>';
+		e.preventDefault();
 	}
-	conectar();
+
+	document.getElementById('boton').addEventListener('click',conectar);
 });
